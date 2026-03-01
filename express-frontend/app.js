8b2b4824073d1fb2043929626b14f66fb5e5d998
vboxuser <shubhamulhalkar2000@gmail.com>
2026-03-01 10:09:57 +0000

    cicd assignment tutedude

---
+++ express-frontend/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send(`
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>express-frontend</title>
+</head>
+<body>
+    <p>Hello from express</p>    
+</body>
+<style>
+    body {
+        background-color: antiquewhite;
+        color: brown;
+        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    }
+</style>
+</html>
+`
+
+    );
+});
+
+app.listen(PORT,"0.0.0.0",()=>{
+    console.log(`Express is running on port ${PORT}`);
+})
